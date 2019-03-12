eb04c161f26d13a04f0682f228741ccf2865d527
CalieR <calielrushton@gmail.com>
2019-03-12 09:28:10 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,21 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    # render the form that collects the data
+    erb :newteam
+  end
+
+  # get '/team' do
+  #  # don't need a get route this time but useful to display params
+  #  erb :newteam
+  # end
+
+  # form is sending the data to here:
+  post '/team' do
+    # pass the submitted data to team.erb template
+    # team.erb processes and handles all the formatting
+    erb :team
+  end
 
 end
